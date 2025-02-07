86d84836673f35529ed0da8f015aee6414a60266
Lorenzo <grobier.2h@gmail.com>
2025-02-07 19:41:20 -0300

    PF4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 function Home() {
   return (
-    <div className="container my-5">
-      <h1 className="text-center">TU SALUD NUESTRA MISION</h1>
-      <p className="text-center">
-        Bienvenido a nuestra tienda en línea. ¡Explora nuestros servicios!
-      </p>
+    <div className="home-container">
+      {/* Ubicación del video: Coloca tu archivo en la carpeta 'public' con el nombre 'video.mp4' */}
+      <video autoPlay loop muted className="background-video">
+        <source src="/video.mp4.mp4" type="video/mp4" />
+        Tu navegador no soporta videos.
+      </video>
+
+      {/* Contenido centrado sobre el video */}
+      <div className="content">
+        <h1>TU SALUD, NUESTRA MISIÓN</h1>
+        <p>Bienvenido a nuestra tienda en línea. ¡Explora nuestros servicios!</p>
+      </div>
     </div>
   );
 }
